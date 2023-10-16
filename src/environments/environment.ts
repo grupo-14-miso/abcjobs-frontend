e0a66a2fc5d6ee4e3e6d9ab6e5ef2be6a5692665
JAEL ALEXANDER RIVERA OVIEDO <ja.riverao1@uniandes.edu.co>
2023-10-15 19:01:14 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const baseUrl = 'https://34.171.225.63.nip.io/';
+const baseUrl = 'http://34.171.225.63.nip.io/';
 
 export const environment = {
   production: false,
