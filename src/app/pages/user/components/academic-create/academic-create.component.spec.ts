e1f7fc19a05c6529f46197fea9052370ae67925d
hawerforerouniandes <h.foreror@uniandes.edu.co>
2023-11-20 16:18:24 -0500

    academic

--- src/app/pages/user/components/academic-create/academic-create.component.spec.ts
+++ src/app/pages/user/components/academic-create/academic-create.component.spec.ts
@@ -4,25 +4,3 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { AcademicCreateComponent } from './academic-create.component';
-
-describe('AcademicCreateComponent', () => {
-  let component: AcademicCreateComponent;
-  let fixture: ComponentFixture<AcademicCreateComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AcademicCreateComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AcademicCreateComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
--- src/app/pages/user/components/academic-list/academic-list.component.spec.ts
+++ src/app/pages/user/components/academic-list/academic-list.component.spec.ts
@@ -4,25 +4,3 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { AcademicListComponent } from './academic-list.component';
-
-describe('AcademicListComponent', () => {
-  let component: AcademicListComponent;
-  let fixture: ComponentFixture<AcademicListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AcademicListComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AcademicListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
